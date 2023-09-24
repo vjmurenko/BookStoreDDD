10234d03fffbfa845ede1ae4d632d82e1b59d6e9
vladimir <vjmurenko@gmail.com>
2023-09-24 21:29:39 +0300

    Добавил кнопку поиска

---
+++ Store.Domain/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace Store.Domain
+{
+    public class Book
+    {
+        public int Id { get; }
+        public string Title { get; }
+
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
